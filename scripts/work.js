260d61f53a6ce38197c7bf8f60d3bcb489d50449
sheriff1 <sheriff.jolaoso@appfolio.com>
2017-07-01 13:48:25 -0700

    change buttons to semantic cards
    bunch o' updates of UI on work page: uses semantic UI cards (responsive), colorized entire background when card is clicked.

--- scripts/work.js
+++ scripts/work.js
@@ -4,17 +4,21 @@ $( document ).ready(function() {
 		$("#gadc-work-content").hide();
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp();
+		$("#main-content").css('background-color','#650104');
 	});
 	$("#gadc-work-button").click(function (){
 		$("#gadc-work-content").show().css('display', 'flex');
 		$("#vtec-work-content").hide();
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp();
+		$("#main-content").css('background-color','#eb1e3e');
 	});
 	$("#appf-work-button").click(function (){
 		$("#appf-work-content").show().css('display', 'flex');
 		$("#gadc-work-content").hide();
 		$("#vtec-work-content").hide();
 		$("#work-profile-hero").slideUp();
+		$("#main-content").css('background-color','#00afdb');
+		console.log("Heh");
 	});
 });
