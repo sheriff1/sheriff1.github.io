1ec3ba6d66408657e3dd14c6ac1a49d72667c30b
sheriff1 <sheriff.jolaoso@appfolio.com>
2017-07-01 11:25:25 -0700

    remove blog section for now, small edits to click methods for work page

--- scripts/work.js
+++ scripts/work.js
@@ -3,18 +3,18 @@ $( document ).ready(function() {
 		$("#vtec-work-content").show().css('display', 'flex');
 		$("#gadc-work-content").hide();
 		$("#appf-work-content").hide();
-		$("#work-profile-hero").hide();
+		$("#work-profile-hero").slideUp();
 	});
 	$("#gadc-work-button").click(function (){
 		$("#gadc-work-content").show().css('display', 'flex');
 		$("#vtec-work-content").hide();
 		$("#appf-work-content").hide();
-		$("#work-profile-hero").hide();
+		$("#work-profile-hero").slideUp();
 	});
 	$("#appf-work-button").click(function (){
 		$("#appf-work-content").show().css('display', 'flex');
 		$("#gadc-work-content").hide();
 		$("#vtec-work-content").hide();
-		$("#work-profile-hero").hide();
+		$("#work-profile-hero").slideUp();
 	});
 });
