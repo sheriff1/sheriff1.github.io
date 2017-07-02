d9fe2eb755351a16c866473161be5bf098b76bee
sheriff1 <sheriff.jolaoso@appfolio.com>
2017-07-01 19:02:04 -0700

    ui tweaks to solidify scaffolding

--- scripts/work.js
+++ scripts/work.js
@@ -5,6 +5,14 @@ $( document ).ready(function() {
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp();
 		$("#main-content").css('background-color','#650104');
+
+		$("#vtec-work-button .work-button-image").css('filter','opacity(1)');
+		$("#vtec-work-button .work-button-label").css('color','inherit');
+
+		$("#appf-work-button .work-button-image").css('filter','opacity(10%)');
+		$("#appf-work-button .work-button-label").css('color','#e6e6e6');
+		$("#gadc-work-button .work-button-image").css('filter','opacity(10%)');
+		$("#gadc-work-button .work-button-label").css('color','#e6e6e6');
 	});
 	$("#gadc-work-button").click(function (){
 		$("#gadc-work-content").show();
@@ -12,6 +20,14 @@ $( document ).ready(function() {
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp();
 		$("#main-content").css('background-color','#e21929');
+
+		$("#gadc-work-button .work-button-image").css('filter','opacity(1)');
+		$("#gadc-work-button .work-button-label").css('color','inherit');
+
+		$("#vtec-work-button .work-button-image").css('filter','opacity(10%)');
+		$("#vtec-work-button .work-button-label").css('color','#e6e6e6');
+		$("#appf-work-button .work-button-image").css('filter','opacity(10%)');
+		$("#appf-work-button .work-button-label").css('color','#e6e6e6');
 	});
 	$("#appf-work-button").click(function (){
 		$("#appf-work-content").show();
@@ -19,5 +35,13 @@ $( document ).ready(function() {
 		$("#vtec-work-content").hide();
 		$("#work-profile-hero").slideUp();
 		$("#main-content").css('background-color','#00afdb');
+
+		$("#appf-work-button .work-button-image").css('filter','opacity(1)');
+		$("#appf-work-button .work-button-label").css('color','inherit');
+
+		$("#vtec-work-button .work-button-image").css('filter','opacity(10%)');
+		$("#vtec-work-button .work-button-label").css('color','#e6e6e6');
+		$("#gadc-work-button .work-button-image").css('filter','opacity(10%)');
+		$("#gadc-work-button .work-button-label").css('color','#e6e6e6');
 	});
 });
