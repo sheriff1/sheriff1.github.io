38c6760c6c70da15fd77a93310e9a722be20246c
sheriff1 <sheriff.jolaoso@appfolio.com>
2017-07-01 21:45:02 -0700

    minor ui tweaks and content updates

--- scripts/work.js
+++ scripts/work.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 		$("#vtec-work-content").show();
 		$("#gadc-work-content").hide();
 		$("#appf-work-content").hide();
-		$("#work-profile-hero").slideUp();
+		$("#work-profile-hero").slideUp('slow');
 		$("#main-content").css('background-color','#650104');
 
 		$("#vtec-work-button .work-button-image").css('filter','opacity(1)');
@@ -18,7 +18,7 @@ $( document ).ready(function() {
 		$("#gadc-work-content").show();
 		$("#vtec-work-content").hide();
 		$("#appf-work-content").hide();
-		$("#work-profile-hero").slideUp();
+		$("#work-profile-hero").slideUp('slow');
 		$("#main-content").css('background-color','#e21929');
 
 		$("#gadc-work-button .work-button-image").css('filter','opacity(1)');
@@ -33,7 +33,7 @@ $( document ).ready(function() {
 		$("#appf-work-content").show();
 		$("#gadc-work-content").hide();
 		$("#vtec-work-content").hide();
-		$("#work-profile-hero").slideUp();
+		$("#work-profile-hero").slideUp('slow');
 		$("#main-content").css('background-color','#00afdb');
 
 		$("#appf-work-button .work-button-image").css('filter','opacity(1)');
