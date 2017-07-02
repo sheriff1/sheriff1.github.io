23e585c81bbd131a67c0697df705f23c5e204dc0
sheriff1 <sheriff.jolaoso@appfolio.com>
2017-07-01 18:16:14 -0700

    further scaffolded w/bootstrap

--- scripts/work.js
+++ scripts/work.js
@@ -1,20 +1,20 @@
 $( document ).ready(function() {
 	$("#vtec-work-button").click(function (){
-		$("#vtec-work-content").show().css('display', 'flex');
+		$("#vtec-work-content").show();
 		$("#gadc-work-content").hide();
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp();
 		$("#main-content").css('background-color','#650104');
 	});
 	$("#gadc-work-button").click(function (){
-		$("#gadc-work-content").show().css('display', 'flex');
+		$("#gadc-work-content").show();
 		$("#vtec-work-content").hide();
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp();
 		$("#main-content").css('background-color','#e21929');
 	});
 	$("#appf-work-button").click(function (){
-		$("#appf-work-content").show().css('display', 'flex');
+		$("#appf-work-content").show();
 		$("#gadc-work-content").hide();
 		$("#vtec-work-content").hide();
 		$("#work-profile-hero").slideUp();
