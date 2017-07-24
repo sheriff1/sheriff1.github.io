72afa332f5baea8c7b29dab659702124b6018ae0
sheriff1 <sheriff.jolaoso@appfolio.com>
2017-07-23 18:15:54 -0700

    back to white bkgnds, added some imgs

--- scripts/work.js
+++ scripts/work.js
@@ -4,13 +4,18 @@ $( document ).ready(function() {
 		$("#gadc-work-content").hide();
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp('slow');
-		$("#main-content").css('background-color','#650104');
 
+		$("#vtec-work-button").css('background-color',"#ffffff");
+		$("#vtec-work-button").css('border-color',"#ffffff");
 		$("#vtec-work-button .work-button-image").css('filter','opacity(1)');
-		$("#vtec-work-button .work-button-label").css('color','inherit');
+		$("#vtec-work-button .work-button-label").css('color','inherit');    
 
+		$("#appf-work-button").css('background-color','#F5F7FA');
+		$("#appf-work-button").css('border-color','#F5F7FA');
 		$("#appf-work-button .work-button-image").css('filter','opacity(15%)');
 		$("#appf-work-button .work-button-label").css('color','#e6e6e6');
+		$("#gadc-work-button").css('background-color','#F5F7FA');
+		$("#gadc-work-button").css('border-color','#F5F7FA');
 		$("#gadc-work-button .work-button-image").css('filter','opacity(15%)');
 		$("#gadc-work-button .work-button-label").css('color','#e6e6e6');
 
@@ -23,15 +28,20 @@ $( document ).ready(function() {
 		$("#vtec-work-content").hide();
 		$("#appf-work-content").hide();
 		$("#work-profile-hero").slideUp('slow');
-		$("#main-content").css('background-color','#e21929');
 
+		$("#gadc-work-button").css('background-color',"#ffffff");
+		$("#gadc-work-button").css('border-color',"#ffffff");
 		$("#gadc-work-button .work-button-image").css('filter','opacity(1)');
 		$("#gadc-work-button .work-button-label").css('color','inherit');
 
+		$("#vtec-work-button").css('background-color','#F5F7FA');
 		$("#vtec-work-button .work-button-image").css('filter','opacity(15%)');
 		$("#vtec-work-button .work-button-label").css('color','#e6e6e6');
+		$("#vtec-work-button").css('border-color','#F5F7FA');
+		$("#appf-work-button").css('background-color','#F5F7FA');
 		$("#appf-work-button .work-button-image").css('filter','opacity(15%)');
 		$("#appf-work-button .work-button-label").css('color','#e6e6e6');
+		$("#appf-work-button").css('border-color','#F5F7FA');
 
 		$("#gadc-work-button").addClass('work-navigation-selected');
 		$("#appf-work-button").removeClass('work-navigation-selected');
@@ -42,15 +52,20 @@ $( document ).ready(function() {
 		$("#gadc-work-content").hide();
 		$("#vtec-work-content").hide();
 		$("#work-profile-hero").slideUp('slow');
-		$("#main-content").css('background-color','#00afdb');
 
+		$("#appf-work-button").css('background-color',"#ffffff");
+		$("#appf-work-button").css('border-color',"#ffffff");
 		$("#appf-work-button .work-button-image").css('filter','opacity(1)');
 		$("#appf-work-button .work-button-label").css('color','inherit');
 
+		$("#vtec-work-button").css('background-color','#F5F7FA');
 		$("#vtec-work-button .work-button-image").css('filter','opacity(15%)');
 		$("#vtec-work-button .work-button-label").css('color','#e6e6e6');
+		$("#vtec-work-button").css('border-color','#F5F7FA');
+		$("#gadc-work-button").css('background-color','#F5F7FA');
 		$("#gadc-work-button .work-button-image").css('filter','opacity(15%)');
 		$("#gadc-work-button .work-button-label").css('color','#e6e6e6');
+		$("#gadc-work-button").css('border-color','#F5F7FA');
 
 		$("#appf-work-button").addClass('work-navigation-selected');
 		$("#gadc-work-button").removeClass('work-navigation-selected');
